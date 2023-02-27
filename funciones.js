ab4268b51767196609df77c13e76902969601a6b
DamianBasa <107730983+DamianBasa@users.noreply.github.com>
2023-02-26 22:24:47 -0300

    Add files via upload

--- funciones.js
+++ funciones.js
@@ -13,7 +13,7 @@ for(let a=0; a<sobreMenuMi.length;a++){
         else if (a==1){
             titulo.innerHTML="Mi fecha de nacimiento"
             //pongo otro html mostrando mi edad
-            texto.textContent="Nací un 11/08/1990, día sábado a las 3:00 de la madrugada aproximadamente"
+            texto.textContent="Nací el  sábado 11 de agosto de 1990,  a las 3:00 de la madrugada aproximadamente"
             texto.style.height="auto";
             
             
@@ -21,7 +21,7 @@ for(let a=0; a<sobreMenuMi.length;a++){
         else if (a==2){
             titulo.innerHTML="Mi EXPERIANCIA LABORAL"
             //mostrar mi experiancia laboral
-            texto.innerHTML="<h2>Community Manager:</h2><h4>AVELLANEDA AUTOMOTORES - AVELLANEDA (SANTA FE) ENERO DE 2013 HASTA LA FECHA</h4><ul><li>Diseño las campañas de Marketing Digital,</li><li>me encargado de la creación de contenidos</li><li> y de hacer crecer la comunidad digital</li></ul><hr><h2>Copistería La Universidad:</h2><h4>COPISTERÍA LA UNIVERSIDAD - SAN FRANCISCO (CÓRDOBA) ENERO DE 2009 HASTA DICIEMBRE DE 2011</h4><ul><li>Atención al público</li><li>Manejo de máquinas fotocopiadoras</li><li>Diseño de flyer</li></ul><hr><h2>DB Diseño – Avellaneda:</h2><h4>ENERO DE 2018 HASTA LA FECHA</h4><ul><li>Actualmente tengo mi emprendimiento en gráfica digital</li><li>Realizó murales con vinilo, impresos o de corte</li><li>Diseño Gráfico y mantenimientos de RRSS</li></ul><hr>"
+            texto.innerHTML="<h2>Community Manager:</h2><h4>AVELLANEDA AUTOMOTORES - AVELLANEDA (SANTA FE) ENERO DE 2013 HASTA LA FECHA</h4><ul><li>Diseño las campañas de Marketing Digital,</li><li>me encargo de la creación de contenidos</li><li> y de hacer crecer la comunidad digital</li></ul><hr><h2>Copistería La Universidad:</h2><h4>COPISTERÍA LA UNIVERSIDAD - SAN FRANCISCO (CÓRDOBA) ENERO DE 2010 HASTA AGOSTO DE 2011</h4><ul><li>Atención al público</li><li>Manejo de máquinas fotocopiadoras</li><li>Diseño de flyer</li></ul><hr><h2>DB Diseño – Avellaneda:</h2><h4>ENERO DE 2018 HASTA LA FECHA</h4><ul><li>Actualmente tengo mi emprendimiento en gráfica digital</li><li>Realizó murales con vinilo, impresos o de corte</li><li>Diseño Gráfico y mantenimientos de RRSS</li></ul><hr>"
             texto.style.height="auto";
         }
         else{
